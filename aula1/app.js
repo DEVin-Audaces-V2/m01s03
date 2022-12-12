34405735b648c9f2b6edf10e44cf8cd5291254cb
romeuic <casarottori@gmail.com>
2022-12-12 20:22:09 -0300

    adiciona exemplo m1-s03-a1

---
+++ aula1/app.js
@@ -0,0 +1,55 @@
+
+//const titulo = document.getElementById("titulo");
+// console.log('Olá, dev!', titulo);
+
+// Exemplo de Ternário
+/*
+const ehVerdade = 'sdaf';
+
+if (ehVerdade) {
+  console.log('VERDADEIRO!');
+} else {
+  console.log('FALSO!');
+}
+
+ehVerdade ? console.log('VERDADEIRO!') : console.log('FALSO!');
+const resultado = 2 + 2 === 4 ? "SIM" : "NAO";
+console.log(resultado);
+*/
+
+// Exemplo de Switch
+/*
+const numero = 2;
+
+if (numero === 0) {
+  console.log('ZERO');
+} else if (numero === 1) {
+  console.log('UM');
+} else if (numero === 2) {
+  console.log('DOIS');
+} else {
+  console.log('OUTRO');
+}
+
+switch (numero) {
+  case 0:
+    console.log('ZERO');
+    break;
+  case 1:
+    console.log('UM');
+    break;
+  case 2:
+    console.log('DOIS');
+    break;
+  default:
+    console.log('OUTRO');
+}
+*/
+
+// Exemplo || / ??
+/*
+const numA = 0;
+const numB = numA || "AAAAAA";
+const numC = numA ?? "AAAAAA";
+console.log(numB, numC);
+*/
