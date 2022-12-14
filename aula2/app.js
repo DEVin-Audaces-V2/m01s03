a4f81b425c614c111fa3b361fe593f01a922a49c
romeuic <casarottori@gmail.com>
2022-12-13 22:01:07 -0300

    atualiza exemplo m1-s03-a2

--- aula2/app.js
+++ aula2/app.js
@@ -53,13 +53,96 @@ const texto = olaNome('Bolacha', 33);
 // console.log(texto);
 
 // Função anônima imediatamente executada
-(function (n) {
-  console.log(`Sou uma função anônima ${n}!`);
+/*
+(function () {
+  console.log(`Sou uma função anônima!`);
 })();
+*/
 
 // Função anônima atribuída a uma variável
-const exemplo = function (n) {
+const variavelComFuncao = function (n) {
   console.log(`Funciono mesmo em uma variável ${n}!`);
 };
+//variavelComFuncao(6666);
 
-exemplo(6666);
+// ARROW FUNCTION
+
+// Arrow function completa
+const vArrowCompleta = (n) => {
+  console.log(`Arrow completa ${n}!`);
+};
+
+// Caso apenas um parametro podemos omitir os parentesis
+const vArrowSemParentesis = n => {
+  console.log(`Arrow sem parentesis ${n}!`);
+};
+
+// Caso apenas uma expressão no corpo podemos
+// omitir inclusive as chaves do bloco de funcao
+const vArrowSemChaves = n => 
+  console.log(`Arrow sem parentesis ${n}!`);
+
+
+// COMPARANDO FUNÇÕES
+// funcao comum
+function fComum(x) {
+  return x * 10000;
+}
+// arrow function completa
+const fArrowC = (x) => {
+  return x * 10000;
+}
+// arrow function sem parentesis
+const fArrowP = x => {
+  return x * 10000;
+}
+// arrow function sem chaves 
+const fArrow = x => x * 10000;
+
+const rC = fComum(3);
+const rA = fArrow(6);
+//console.log(rC);
+// console.log(rA);
+
+
+// Exemplo de diferenças de contexto this
+const objeto = {
+  nome: "Teste",
+  nomeDaFuncao: function () {
+    // este this é o objeto
+    console.log(this);
+    console.log(window === this);
+  },
+  nomeDaArrow: () => {
+    // este this é o contexto global window
+    console.log(this);
+    console.log(window === this);
+  }
+};
+//objeto.nomeDaFuncao();
+//objeto.nomeDaArrow();
+
+const vetorB = ['A', 'B', 'C'];
+
+console.log(vetorB);
+
+for (let i = 0; i < vetorB.length; i++) {
+  console.log(`i: ${i} v: ${vetorB[i]}`);
+}
+
+// exemplo de funcao comum como parametro
+vetorB.forEach(function (valor) {
+  console.log(`v: ${valor}`);
+});
+
+// exemplo de arrow function como parametro
+vetorB.forEach(valor => console.log(`v: ${valor}`));
+
+
+function calculaPontoA(x, y, z) {
+  return [x * 2, y * 2, z * 2];
+}
+
+const calculaPontoB = (x, y, z) => [x * 2, y * 2, z * 2];
+
+console.log(calculaPontoB(1, 2, 3));
